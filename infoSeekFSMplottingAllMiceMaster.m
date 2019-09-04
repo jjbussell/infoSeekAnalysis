eb6773616146ca75ad388933267fc385c452560e
Jennifer Bussell <jbussell@gmail.com>
2019-09-03 21:20:19 -0400

    plotting

--- infoSeekFSMplottingAllMiceMaster.m
+++ infoSeekFSMplottingAllMiceMaster.m
@@ -1643,16 +1643,16 @@ ax.XLim = [0 1];
 ax.YLim = [0 1];
 for mm = 1:numel(a.reverseMice)
     m = a.reverseMice(mm);
-    text(1-a.incomplete(m,6),a.overallChoice(m,5) + 0.01,a.reverseMiceList{mm},'HorizontalAlignment','center');
+    text(a.incomplete(m,6),a.overallChoice(m,5) + 0.01,a.reverseMiceList{mm},'HorizontalAlignment','center');
 end
-scatter(1-a.incomplete(a.reverseMice,6),a.overallChoice(a.reverseMice,5),'filled')
+scatter(a.incomplete(a.reverseMice,6),a.overallChoice(a.reverseMice,5),'filled')
 plot([-10000000 1000000],[0.5 0.5],'color',[0.2 0.2 0.2],'linewidth',0.25,'yliminclude','off','xliminclude','off');
 plot([0.5 0.5],[-10000000 1000000],'color',[0.2 0.2 0.2],'linewidth',0.25,'yliminclude','off','xliminclude','off');
 plot([0 1],[0 1],'color',[0.2 0.2 0.2],'linewidth',0.25,'yliminclude','off','xliminclude','off');
 plot([0 1],[1 0],'color',[0.2 0.2 0.2],'linewidth',0.25,'yliminclude','off','xliminclude','off');
 ylabel({'P(choose info)'}); %{'Info choice', 'probability'}
-xlabel({'P(Present in port on info small)'});
-title('Raw choice percentages, by physical info side');
+xlabel({'P(NOT present in port on info small)'});
+title('Choice of information vs. probability of leaving on low-value info trials');
 hold off;
 
 saveas(fig,fullfile(pathname,'Prefbyleaving'),'pdf');
